a2471049c236c0850b121ffcfbf92dc2d2a5e36b
bao2703 <bao2703@gmail.com>
2017-03-18 13:22:24 +0700

    Initial commit

---
+++ src/com/neptune/Main.java
@@ -0,0 +1,8 @@
+package com.neptune;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/neptune/State.java
@@ -0,0 +1,8 @@
+package com.neptune;
+
+/**
+ * Created by Neptune on 3/18/2017.
+ */
+public class State {
+    
+}
